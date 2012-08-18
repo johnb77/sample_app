5e8cce1ed3e071e5dd54f2f093cf5125799aa550
John <johnb77@gmail.com>
2012-08-18 13:36:38 -0400

    set asset.compile to true to for heroku error fix, this is a temp fix which is not ideal as performace is effected

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ SampleApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true	#jb: not good for performance, only use for dev/testing 
 
   # Generate digests for assets URLs
   config.assets.digest = true
