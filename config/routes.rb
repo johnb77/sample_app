a1478cbaa9f873da112aa5cd88e54cc1194b8407
John <johnb77@gmail.com>
2012-08-27 20:41:38 -0400

    ch 5 in progress

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ SampleApp::Application.routes.draw do
 
   get "pages/help"
 
+  root :to => "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
